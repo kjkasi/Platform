ba799743a5ba8f884bd0e7fae9b126734a8217bb
kjkasi <root@localhost.localdomain>
2022-09-15 19:30:40 +0300

    No message

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnetion()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+            return Ok("Inbound test of from Platforms Controller");
+        }
+    }
+}
